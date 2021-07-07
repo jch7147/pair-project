b0e4b8a0379afdb4faf869d1141aa29a72da1c63
id <id@IN0013421>
2021-07-07 16:14:23 +0900

    07/07 16:13
    @GeneratedValueの追加
    など

--- src/main/java/com/example/demo/User_info.java
+++ src/main/java/com/example/demo/User_info.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -8,6 +10,7 @@ import javax.persistence.Table;
 @Table(name = "user_info")
 public class User_info {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private int job_code;
 	private String name;
