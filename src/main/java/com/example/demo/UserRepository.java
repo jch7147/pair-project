f2f72324d44db688f703ab32068143469b97c19e
id <id@IN0013421>
2021-07-07 14:18:59 +0900

    07/07 14:18
    コントローラーの作成

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User_info, Integer> {
-
+	List<User_info> findByEmailAndPassword(String email, String password);
 }
